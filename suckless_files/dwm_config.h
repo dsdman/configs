41cfaa85aec899877675776e4f632fb0f313746b
dsdman <desaidylan123@gmail.com>
2018-01-09 21:53:55 -0500

    Update dwm, zsh, and nvim configs

--- suckless_files/dwm_config.h
+++ suckless_files/dwm_config.h
@@ -8,8 +8,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "DejaVuSansMonoPowerlinePatched:size=11" };
-static const char dmenufont[]       = "DejaVuSansMonoPowerlinePatched:size=10";
+static const char *fonts[]          = { "DejaVu Sans Mono:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_black[]       = "#000000";
 static const char col_orangered[]   = "#8b2500";
@@ -17,14 +17,13 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_darkred[]     = "#870505";
-//static const char col_darkbrown[]   = "#885A67";
+static const char col_firebrick[]   = "#B22222";
 static const char col_darkbrown[]   = "#A96A68";
-static const char col_firebrick[]   = "#B50000";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_black, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_darkbrown, col_darkbrown },
+	[SchemeSel]  = { col_gray4, col_black, col_black },
 };
 
 /* tagging */
@@ -42,19 +41,22 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
-/*static const Layout layouts[] = {
+/*
+static const Layout layouts[] = {
 	{ "[]=",      tile },    
 	{ "><>",      NULL },    
 	{ "[M]",      monocle },
 };*/
 
+#include "grid.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[T]",      tile },    /* first entry is default */
 	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[G]",      grid },
 };
 
 /* key definitions */
@@ -72,10 +74,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray3, "-sb", col_darkbrown, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browser[]  = { "firefox", NULL };
 static const char *editor[]  = { "st", "-e", "nvim", NULL };
 static const char *calc[]  = { "speedcrunch", NULL };
 static const char *filebrowser[]  = { "pcmanfm-qt", NULL };
-static const char *browser[]  = { "firefox", NULL };
 static const char *screenshot[]  = { "scrot",  NULL };
 static const char *poweroff[] = { "systemctl", "poweroff", NULL};
 static const char *suspend[] = { "systemctl", "suspend", NULL};
@@ -97,10 +99,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Right,  moveresize,     {.v = "0x 0y 25w 0h" } },
 	{ MODKEY|ShiftMask,             XK_Left,   moveresize,     {.v = "0x 0y -25w 0h" } },
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_y,      spawn,          {.v = editor} },
+  { MODKEY,                       XK_y,      spawn,          {.v = editor} },
+  { MODKEY,                       XK_e,      spawn,          {.v = filebrowser } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = browser } },
-	{ MODKEY,                       XK_e,      spawn,          {.v = filebrowser } },
-	{ MODKEY,                       XK_c,      spawn,          {.v = calc } },
+  { MODKEY,                       XK_c,      spawn,          {.v = calc } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -115,6 +117,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
